85a0de5f25ef25ccf1d8fe579cf465ce7003fa3d
Alex Knott <alextknott@gmail.com>
2022-03-17 17:14:53 +0000

    Make ship an initialized variable at start, and add a shipHit test

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,5 +1,12 @@
 const { shipFactory } = require("../modules/ship");
 
+const ship = shipFactory(3);
+
 test("shipLength", () => {
-  expect(shipFactory(2).shipLength).toBe(2);
+  expect(ship.shipLength).toBe(3);
+});
+
+test("shipHit", () => {
+  ship.hit(0);
+  expect(ship.hitArray[0].toBe("hit"));
 });
