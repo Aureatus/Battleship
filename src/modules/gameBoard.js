417aec1e086bb735fe857edd37ccce3599671b48
Alex Knott <alextknott@gmail.com>
2022-03-19 16:54:43 +0000

    Add conditional check for valid horizontal  placement before going ahead with ship creation + placement

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -4,15 +4,22 @@ const gameBoardFactory = () => {
   const gameBoardArray = new Array(100).fill("");
   const ships = [];
   const placeShip = (position, length, tag, orientation) => {
-    ships.push(shipFactory(length));
-
     if (orientation === "horizontal") {
+      for (i = position; i < position + length; i++) {
+        if (gameBoardArray[i] != "") {
+          throw new Error(
+            `You are trying to place a ship where there is already one.`
+          );
+        }
+      }
+      ships.push(shipFactory(length));
       for (i = position; i < position + length; i++) {
         gameBoardArray[i] = tag;
       }
     }
     if (orientation === "vertical") {
       let counter = position;
+      ships.push(shipFactory(length));
       for (i = position; i < position + length; i++) {
         gameBoardArray[counter] = tag;
         counter += 10;
