6ff9545002206f3a6827ee88b763ab53a65c8060
Alex Knott <alextknott@gmail.com>
2022-03-20 15:32:54 +0000

    Add logic for attackEnemyGameboard for player of type human

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,3 +1,11 @@
-const player = () => {};
+const player = (gameboard, enemygameboard, type) => {
+  if (type === "human") {
+    const attackEnemyGameboard = (x, y) => {
+      let position = gameboard.coordinatesToIndex(x, y);
+      enemygameboard.receiveAttack(position);
+    };
+    return { attackEnemyGameboard };
+  }
+};
 
-module.exports = player;
+exports.player = player;
