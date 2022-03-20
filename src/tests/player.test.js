f32209c686a777f5f16b3702df7fdb4c37769b37
Alex Knott <alextknott@gmail.com>
2022-03-20 15:27:04 +0000

    Implement test for attackEnemyGameboard with human player

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,1 +1,10 @@
+const { gameBoardFactory } = require("../modules/gameBoard");
 const { player } = require("../modules/player");
+
+test("attackEnemyGameboardHuman", () => {
+  const gameboard1 = gameBoardFactory();
+  const gameboard2 = gameBoardFactory();
+  const player1 = player(gameboard1, gameboard2, "human");
+  player1.attackEnemyGameboard(5);
+  expect(gameboard2.gameBoardArray[5]).toBe("miss");
+});
