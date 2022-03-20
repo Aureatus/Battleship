bf8ff9ab421a4a35253ae92b857b53313c64d69a
Alex Knott <alextknott@gmail.com>
2022-03-20 16:12:07 +0000

    Implement test for attackEnemyGameboard where type of player is a computer

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -8,3 +8,11 @@ test("attackEnemyGameboardHuman", () => {
   player1.attackEnemyGameboard(5);
   expect(gameboard2.gameBoardArray[5]).toBe("miss");
 });
+
+test("attackEnemyGameboardComputer", () => {
+  const gameboard1 = gameBoardFactory();
+  const gameboard2 = gameBoardFactory();
+  const player1 = player(gameboard1, gameboard2, "computer");
+  let position = player1.attackEnemyGameboard();
+  expect(gameboard2.gameBoardArray[position]).toBe("miss");
+});
