4b8e6bd7d3b3d44bba578ed0f16f2c6af44913da
Alex Knott <alextknott@gmail.com>
2022-03-21 18:24:09 +0000

    Add function that adds event listeners to each grid element, and sends an attack to it's respective array element on click

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -2,14 +2,21 @@ const domMethods = () => {
   const body = document.body;
   const generateGrid = (gameBoard, number) => {
     const grid = document.querySelector("main").children[number].children[0];
-    console.log(grid);
     gameBoard.gameBoardArray.forEach((e) => {
       const div = document.createElement("div");
       div.textContent = e;
       grid.appendChild(div);
     });
   };
-  return { generateGrid };
+  const attackListener = (gameBoard, number, attackEnemyGameboard) => {
+    const grid = document.querySelector("main").children[number].children[0];
+    Array.from(grid.children).forEach((element, index) => {
+      element.addEventListener("click", () => {
+        attackEnemyGameboard(undefined, undefined, index);
+      });
+    });
+  };
+  return { generateGrid, attackListener };
 };
 
 exports.domMethods = domMethods;
