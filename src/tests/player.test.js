21f54406cf4f5dc0421253eb3d3dbfbf8d7ab3ba
Alex Knott <alextknott@gmail.com>
2022-03-21 17:59:50 +0000

    Add test for attackEnemyGameboardHumanHit and created gameboard1 and 2 before each test

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,17 +1,26 @@
 const { gameBoardFactory } = require("../modules/gameBoard");
 const { player } = require("../modules/player");
+let gameboard1;
+let gameboard2;
+beforeEach(() => {
+  gameboard1 = gameBoardFactory();
+  gameboard2 = gameBoardFactory();
+});
 
-test("attackEnemyGameboardHuman", () => {
-  const gameboard1 = gameBoardFactory();
-  const gameboard2 = gameBoardFactory();
+test("attackEnemyGameboardHumanMiss", () => {
   const player1 = player(gameboard1, gameboard2, "human");
   player1.attackEnemyGameboard(0, 0);
   expect(gameboard2.gameBoardArray[0]).toBe("miss");
 });
 
+test("attackEnemyGameboardHumanHit", () => {
+  const player1 = player(gameboard1, gameboard2, "human");
+  gameboard2.placeShip(0, 2, "P", "horizontal");
+  player1.attackEnemyGameboard(0, 0);
+  expect(gameboard2.gameBoardArray[0]).toBe("hit");
+});
+
 test("attackEnemyGameboardComputer", () => {
-  const gameboard1 = gameBoardFactory();
-  const gameboard2 = gameBoardFactory();
   const player1 = player(gameboard1, gameboard2, "computer");
   let position = player1.attackEnemyGameboard();
   expect(gameboard2.gameBoardArray[position]).toBe("miss");
