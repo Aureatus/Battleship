5d1b05006d65047d6d4247a276a810a004ed9b60
Alex Knott <alextknott@gmail.com>
2022-03-22 14:53:58 +0000

    Make attackListener change textContent of clicked grid to new index content rather than generating entire grid again

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -2,6 +2,10 @@ const domMethods = () => {
   const body = document.body;
   const generateGrid = (gameBoard, number) => {
     const grid = document.querySelector("main").children[number].children[0];
+    console.log(grid.children.length);
+    if (grid.children.length > 0) {
+      Array.from(grid.children).forEach((e) => e.remove());
+    }
     gameBoard.gameBoardArray.forEach((e) => {
       const div = document.createElement("div");
       div.textContent = e;
@@ -12,8 +16,13 @@ const domMethods = () => {
     const grid = document.querySelector("main").children[number].children[0];
     Array.from(grid.children).forEach((element, index) => {
       element.addEventListener("click", () => {
+        let unalteredGameBoard = gameBoard.gameBoardArray.slice();
         attackEnemyGameboard(undefined, undefined, index);
-        generateGrid(gameBoard, number);
+        unalteredGameBoard.forEach((element, index) => {
+          if (unalteredGameBoard[index] != gameBoard.gameBoardArray[index]) {
+            grid.children[index].textContent = gameBoard.gameBoardArray[index];
+          }
+        });
       });
     });
   };
