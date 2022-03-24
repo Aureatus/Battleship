fd703bfea69f891ad6010199a3d4b8ebe18334ed
Alex Knott <alextknott@gmail.com>
2022-03-24 15:29:14 +0000

    Move placeShipsAI function to player module rather main.js and call it as part of gameFactory()

--- src/modules/player.js
+++ src/modules/player.js
@@ -23,7 +23,107 @@ const player = (gameboard, enemygameboard, type) => {
       enemygameboard.receiveAttack(position);
       return position;
     };
-    return { attackEnemyGameboard };
+
+    const placeShipsAI = () => {
+      let shipLetter;
+      let shipLength;
+      let shipOrientation = "horizontal";
+      let coordinates = [];
+      const coordinateGenerator = () => {
+        let verificationArray = new Array(100).fill("");
+        let a = 0;
+        while (a < 5) {
+          switch (a) {
+            case 0:
+              shipLetter = "C";
+              shipLength = 5;
+              break;
+            case 1:
+              shipLetter = "B";
+              shipLength = 4;
+              break;
+            case 2:
+              shipLetter = "D";
+              shipLength = 3;
+              break;
+            case 3:
+              shipLetter = "S";
+              shipLength = 3;
+              break;
+            case 4:
+              shipLetter = "P";
+              shipLength = 2;
+              break;
+            default:
+              break;
+          }
+          const coordObject = {};
+          coordObject.x = Math.floor(Math.random() * 9);
+          coordObject.y = Math.floor(Math.random() * 9);
+          let shipPosition = gameboard.coordinatesToIndex(
+            coordObject.x,
+            coordObject.y
+          );
+          let passesIf = true;
+          if (shipOrientation === "horizontal") {
+            for (let i = shipPosition; i < shipPosition + shipLength; i++) {
+              if (verificationArray[i] != "") {
+                passesIf = false;
+              }
+              let currentTenth = Math.floor(shipPosition / 10) * 10;
+              if (shipPosition + shipLength > currentTenth + 10) {
+                passesIf = false;
+              }
+            }
+            if (passesIf) {
+              for (let i = shipPosition; i < shipPosition + shipLength; i++) {
+                verificationArray[i] = shipLetter;
+              }
+              coordinates.push(coordObject);
+              a++;
+            }
+          }
+        }
+      };
+      coordinateGenerator();
+      let counter = 0;
+      for (counter; counter < 5; counter++) {
+        switch (counter) {
+          case 0:
+            shipLetter = "C";
+            shipLength = 5;
+            break;
+          case 1:
+            shipLetter = "B";
+            shipLength = 4;
+            break;
+          case 2:
+            shipLetter = "D";
+            shipLength = 3;
+            break;
+          case 3:
+            shipLetter = "S";
+            shipLength = 3;
+            break;
+          case 4:
+            shipLetter = "P";
+            shipLength = 2;
+            break;
+          default:
+            break;
+        }
+        gameboard.placeShip(
+          gameboard.coordinatesToIndex(
+            coordinates[counter].x,
+            coordinates[counter].y
+          ),
+          shipLength,
+          shipLetter,
+          "horizontal"
+        );
+      }
+    };
+    return { attackEnemyGameboard, placeShipsAI };
   }
 };
 
