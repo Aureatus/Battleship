74d5b19457e6dd9543cbd5124515cc434fb94e17
Alex Knott <alextknott@gmail.com>
2022-11-03 13:07:42 +0000

    Extract gridChangeRender into it's own module

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -2,6 +2,7 @@ import generateGrid from "./domMethods/generateGrid";
 import removeAllChildren from "./domMethods/removeAllChildren";
 import clearshipPlacementInterfaceGenerator from "./domMethods/clearShipPlacementInterfaceGenerator";
 import gameEndScreen from "./domMethods/gameEndScreen";
+import gridChangeRender from "./domMethods/gridChangeRender";
 
 const domMethods = () => {
   const shipPlacement = (
@@ -172,29 +173,8 @@ const domMethods = () => {
     });
   };
 
-  const gridChangeRender = (gameBoard, number, unalteredGameBoard) => {
-    unalteredGameBoard.forEach((element, index) => {
-      const grid = document.querySelector("main").children[number].children[0];
-      if (unalteredGameBoard[index] != gameBoard.gameBoardArray[index]) {
-        if (number === 1) {
-          grid.children[index].textContent = gameBoard.gameBoardArray[index];
-        }
-        if (gameBoard.gameBoardArray[index] === "hit") {
-          grid.children[index].classList.add("hit");
-          if (number === 1) {
-            grid.children[index].innerHTML = "&#215";
-          }
-        }
-        if (gameBoard.gameBoardArray[index] === "miss") {
-          grid.children[index].classList.add("miss");
-          grid.children[index].innerHTML = "&#215";
-        }
-      }
-    });
-  };
   return {
     attackListener,
-    gridChangeRender,
     shipPlacement,
   };
 };
---
+++ src/modules/domMethods/gridChangeRender.js
@@ -0,0 +1,22 @@
+const gridChangeRender = (gameBoard, number, unalteredGameBoard) => {
+  unalteredGameBoard.forEach((element, index) => {
+    const grid = document.querySelector("main").children[number].children[0];
+    if (unalteredGameBoard[index] != gameBoard.gameBoardArray[index]) {
+      if (number === 1) {
+        grid.children[index].textContent = gameBoard.gameBoardArray[index];
+      }
+      if (gameBoard.gameBoardArray[index] === "hit") {
+        grid.children[index].classList.add("hit");
+        if (number === 1) {
+          grid.children[index].innerHTML = "&#215";
+        }
+      }
+      if (gameBoard.gameBoardArray[index] === "miss") {
+        grid.children[index].classList.add("miss");
+        grid.children[index].innerHTML = "&#215";
+      }
+    }
+  });
+};
+
+export default gridChangeRender;
